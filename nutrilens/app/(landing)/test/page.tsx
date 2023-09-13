24eca91b6c7e4090d8bcee0ffca4eb23d70e73d0
LukasWeigl <63125168+LukasWeigl@users.noreply.github.com>
2023-09-13 16:43:03 +0200

    added skeleton

--- nutrilens/app/(landing)/test/page.tsx
+++ nutrilens/app/(landing)/test/page.tsx
@@ -1,10 +1,35 @@
-const Mewo = () => {
-  
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+const Landing = () => {
   return (
-    <>
-      <p>Hello world</p>
-    </>
+    <div className="flex flex-col items-center justify-center mt-10">
+      <div className="flex flex-col items-start">
+        <Select>
+          <SelectTrigger className="w-[380px]">
+            <SelectValue placeholder="Select Product" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="preWorkout">Pre-Workout</SelectItem>
+            <SelectItem value="proteinPulver">Protein Pulver</SelectItem>
+            <SelectItem value="mealReplacementShake">
+              Meal Replacement Shake
+            </SelectItem>
+          </SelectContent>
+        </Select>
+        <Textarea className="mt-5 mb-5 w-[500px] h-[400px]" />
+        <div></div>
+        <Button>Submit</Button>
+      </div>
+    </div>
   );
 };
 
-export default Mewo;
+export default Landing;
