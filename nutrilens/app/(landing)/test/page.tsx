aef0256ed76c5159229684abf1b8379470aa88ca
LukasWeigl <63125168+LukasWeigl@users.noreply.github.com>
2023-09-17 22:17:01 +0200

    added functional api call

--- nutrilens/app/(landing)/test/page.tsx
+++ nutrilens/app/(landing)/test/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import {
   Select,
@@ -7,14 +9,43 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 
 const Landing = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<String[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [textAreaContent, setTextAreaContent] = useState("");
+
+
+  //const isLoading
+  const onSubmit = async () => {
+    
+
+    try {
+      const userMessage: String = `${selectedProduct}: ${textAreaContent}`
+     
+      //const newMessages = [...messages, userMessage];
+      console.log(userMessage)
+      const response = await axios.post("/api", { messages: userMessage });
+      setMessages((current) => [...current, userMessage, response.data]);
+    } catch (error: any) {
+      //TODO: Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       <div className="flex flex-col items-start">
-        <Select>
+        <Select onValueChange={setSelectedProduct}>
           <SelectTrigger className="w-[380px]">
-            <SelectValue placeholder="Select Product" />
+            <SelectValue  placeholder="Select Product" />
           </SelectTrigger>
           <SelectContent>
             <SelectItem value="preWorkout">Pre-Workout</SelectItem>
@@ -24,12 +55,18 @@ const Landing = () => {
             </SelectItem>
           </SelectContent>
         </Select>
-        <Textarea className="mt-5 mb-5 w-[500px] h-[400px]" />
+        <Textarea
+          className="mt-5 mb-5 w-[500px] h-[400px]"
+          value={textAreaContent}
+          onChange={(e) => setTextAreaContent(e.target.value)}
+        />
         <div></div>
-        <Button>Submit</Button>
+        <Button onClick={onSubmit}>Submit</Button>
       </div>
     </div>
   );
 };
+
+
 
 export default Landing;
--- nutrilens/app/api/route.ts
+++ nutrilens/app/api/route.ts
@@ -1,32 +1,21 @@
 import { NextResponse } from "next/server";
-import OpenAI from 'openai';
-
-
-
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
+const instructionMessage: String =
+  "You are a nutrition expert, you receiving User Information about a certain product with ingredients and dosage usage. You will list all ingredients with their benefits and if the dosage is right. At the end you will a 1-10 Rating for the product. You answer in the language of the user.";
 
-
-const instructionMessage: String = "You are a code generator. You must answer only in markdown code snippest. Use Code Comments for explanations."
-
-
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   try {
-  
     const body = await req.json();
-    const { messages  } = body;
-
-
+    const { messages } = body;
+    console.log("Messages: "+messages)
     if (!messages) {
       return new NextResponse("Messages are required", { status: 400 });
     }
-
-
 
     //const response = await openai.createChatCompletion({
     //  model: "gpt-3.5-turbo",
@@ -35,15 +24,13 @@ export async function POST(
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-
-      messages: [{"role": "system", "content": instructionMessage, ...messages}],
+      messages: [{ role: "system", content: instructionMessage+ " User Information: " +messages }],
     });
     console.log(response.choices[0].message);
 
-
     return NextResponse.json(response);
   } catch (error) {
-    console.log('[CODE_ERROR]', error);
+    console.log("[CODE_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-};
+}
